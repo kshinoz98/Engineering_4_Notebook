871140693f6fe545a13a475d81e237fbec202e29
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-09-07 10:12:19 -0400

    Commented Coundown Code

--- raspberry-pi/Countdown_Launch1.py
+++ raspberry-pi/Countdown_Launch1.py
@@ -1,13 +1,13 @@
 # type: ignore
-import time
+import time         #[1-2] Import neccesary libraries
 import board
 
-count = 10
+count = 10          # Set the time to 10 seconds
 
-while count > 0:
+while count > 0:    #[7-11] Count down and print if the time is greater than 0
     time.sleep(1)
     print(count)
     count = count - 1
 else:
-    time.sleep(1)
+    time.sleep(1)   #[12-13] If the time is 0, print launch
     print("lauch")
