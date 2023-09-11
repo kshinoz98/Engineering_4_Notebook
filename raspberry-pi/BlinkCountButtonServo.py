93b84465cf5d6dbf811f6dc99a597c78b416db1d
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-09-11 10:46:41 -0400

    THidnfskjir

--- raspberry-pi/BlinkCountButtonServo.py
+++ raspberry-pi/BlinkCountButtonServo.py
@@ -2,6 +2,9 @@
 import board                                    #[2-4] Import neccesary libraries
 import time
 import digitalio
+import pwmio
+from adafruit_motor import servo
+pwm_servo = pwmio.PWMOut(board.GP6, duty_cycle=2 ** 15, frequency=50)
 redLED = digitalio.DigitalInOut(board.GP0)      #[5-8] Setup LEDs
 greenLED = digitalio.DigitalInOut(board.GP2)
 button = digitalio.DigitalInOut(board.GP3)
@@ -9,7 +12,9 @@ redLED.direction = digitalio.Direction.OUTPUT
 greenLED.direction = digitalio.Direction.OUTPUT
 button.direction = digitalio.Direction.INPUT
 button.pull = digitalio.Pull.UP
+servo1 = servo.Servo(pwm_servo, min_pulse=500, max_pulse=2500)
 
+servo1.angle = 0
 count = 0                                      #Set the time to 10 seconds
 
 while True:
@@ -22,12 +27,12 @@ while True:
         time.sleep(.5)
         redLED.value = False
         time.sleep(.5)
-        if button.value == False:
-            button.value = 3
-            print("aborted")
         if count == 0:                              #[19-21] If the countdown is 0, print launch, and turn on the green LED
             print("launch")
             greenLED.value = True
+            servo1.angle = 90
             time.sleep(5)                           #Keeps the board alive
             greenLED.value = False
             print("launched")
+            servo1.angle = 0
+
