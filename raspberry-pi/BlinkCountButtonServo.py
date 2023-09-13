38617119908939d53a19a512f321a2eefaf4c5a5
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-09-13 10:45:06 -0400

    Servo now spicy

--- raspberry-pi/BlinkCountButtonServo.py
+++ raspberry-pi/BlinkCountButtonServo.py
@@ -20,19 +20,29 @@ count = 0                                      #Set the time to 10 seconds
 while True:
     if button.value == False:
         count = 10
-    while count > 0:                                #[12-18] Count down and blink red LED if the time is greater than 0
+    while count > 3:                                #[12-18] Count down and blink red LED if the time is greater than 0
         print(count)
         redLED.value = True   
         count = count - 1
         time.sleep(.5)
         redLED.value = False
         time.sleep(.5)
+    while count > 0 & count <= 3:
+        print(count)
+        redLED.value = True
+        count = count - 1
+        for x in range(10):
+            servo1.angle = servo1.angle + 3
+            time.sleep(.05)
+        redLED.value = False
+        for x in range(10):
+            servo1.angle = servo1.angle + 3
+            time.sleep(.05)
         if count == 0:                              #[19-21] If the countdown is 0, print launch, and turn on the green LED
             print("launch")
             greenLED.value = True
-            servo1.angle = 90
-            time.sleep(5)                           #Keeps the board alive
+            servo1.angle = 180
+            time.sleep(5)
             greenLED.value = False
             print("launched")
             servo1.angle = 0
-
