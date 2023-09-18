e29c4e4d8790a125ad65350a784069b0c5ea56d1
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-09-18 10:02:52 -0400

    Commented accelerometer code

--- raspberry-pi/Accelerometer.py
+++ raspberry-pi/Accelerometer.py
@@ -1,13 +1,15 @@
-# type: ignore
-import adafruit_mpu6050
+#type:ignore
+import adafruit_mpu6050                     #[2-5] Import necessary libraries
 import busio
 import board                        
 import time
-sda_pin = board.GP14
+sda_pin = board.GP14                        #[6-9] Set up SDA and SCL pints
 scl_pin = board.GP15
-i2c = busio.I2C(GP15,GP14)
+i2c = busio.I2C(scl_pin,sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 
-while True:
-    print(mpu.acceleration)
-    time.sleep(.05)
+while True:                                 #[11-15] Print necessary aspects of acceleration
+    print(f"x={mpu.acceleration[0]}.")          
+    print(f"y={mpu.acceleration[1]}.")
+    print(f"z={mpu.acceleration[2]}.\n")
+    time.sleep(.2   5)
