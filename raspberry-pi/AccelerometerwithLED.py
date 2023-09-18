40a11c74a013aa9f56512fa7e4815b3b69a6bddb
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-09-18 10:41:02 -0400

    fixed code

--- raspberry-pi/AccelerometerwithLED.py
+++ raspberry-pi/AccelerometerwithLED.py
@@ -3,17 +3,16 @@ import busio
 import board                        
 import time
 import digitalio
-sda_pin = board.GP14                        #[6-9] Set up SDA and SCL pints
+sda_pin = board.GP14                        #[6-11] Set up accelerometer, LED, SDA and SCl pins.
 scl_pin = board.GP15
 i2c = busio.I2C(scl_pin,sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 led = digitalio.DigitalInOut(board.GP0)
 led.direction = digitalio.Direction.OUTPUT
 
-while True: 
-    if mpu.acceleration[1] > 8 or mpu.acceleration[0] > 8 or mpu.acceleration[1] < -8 or mpu.acceleration[0] < -8:                                 #[11-15] Print necessary aspects of acceleration
-        print(f"x={mpu.acceleration[0]}.")          
-        print(f"y={mpu.acceleration[1]}.")
-        print(f"z={mpu.acceleration[2]}.\n")
+while True:                                 #[13-16] If the accelerometer is tilted more than 90 degrees, turn the light on
+    if mpu.acceleration[2] < 1:      
         led.value = True
         time.sleep(.25)
+    else:                                   #[17-18] If the accelerometer is not tilted more than 90 degrees, turn the light off
+        led.value = False
