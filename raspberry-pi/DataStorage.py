a882109d4ccb0c7f7ced74596fe85b7f32f420a9
kshinoz98 <kshinoz98@charlottesvilleschools.org>
2023-12-04 10:14:24 -0500

    commented Datastorage code

--- raspberry-pi/DataStorage.py
+++ raspberry-pi/DataStorage.py
@@ -10,21 +10,27 @@ i2c = busio.I2C(scl_pin,sda_pin)
 mpu = adafruit_mpu6050.MPU6050(i2c)
 led = digitalio.DigitalInOut(board.GP2)
 led.direction = digitalio.Direction.OUTPUT
+printed = False
 
-with open("/data.csv", "a") as datalog:
-    while True:
-            t = time.monotonic()
-            x_accel = mpu.acceleration[0]
-            y_accel = mpu.acceleration[1]
-            z_accel = mpu.acceleration[2]
-            if z_accel < .5:
-                led = True      
-                tilt = "Tilted"
-            else:
-                led = False
-                tilt = "Untilted"
+while True:                                 #[15-25]If the accererometer is tilted, then turn on the light
+    t = time.monotonic()
+    x_accel = mpu.acceleration[0]
+    y_accel = mpu.acceleration[1]
+    z_accel = mpu.acceleration[2]
+    if z_accel < .5:
+        led.value = True     
+        tilt = "Tilted"
+    else:
+        led.value = False
+        tilt = "Untilted"
+    try:                                   #[26-33]If the Pico is in data mode, save the time; x, y, and z accel; and if the servo is tilted
+        with open("/data.csv", "a") as datalog:
             datalog.write(f"{t},{x_accel},{y_accel},{z_accel},{tilt}\n")
-            led = True
             datalog.flush()
-            time.sleep(0.25)
-            led = False
+    except:
+        if printed == False:
+            print("data not open")
+            printed = True
+    led.value = True
+    time.sleep(0.25)
+    led.value = False
